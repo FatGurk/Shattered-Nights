cbe0133ec30e5bd803180b8f1a4f2360845bbca3
Remim01 <kimgustafsson.bjornberg@elev.ga.ntig.se>
2025-11-03 15:15:25 +0100

    :heart_eyes:

--- Game/GameLoop.js
+++ Game/GameLoop.js
@@ -1,8 +1,14 @@
 const canvas = document.getElementById("#GameCanvas");
 const ctx = canvas.getContext("2d");
 
+function Drawscene() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    CharacterList.forEach(e => {
+        e.draw();
+    });
+}
 
-
-gameLooop = {
-
+function gameLoop() {
+    drawScene();
+    requestAnimationFrame(gameLoop);
 }
--- Game/ObjectLists.js
+++ Game/ObjectLists.js
@@ -1,10 +1,12 @@
 import "./Characters/Player.js";
 import "./Characters/Npc.js";
 
-CharacterList = [
-    new Player(50, 50, 50, 50, "Pöjk", PöjkImage),
+export const CharacterList = [
+    new Player(50, 50, 50, 50, "Pöjk", Pojk.png),
+    /*
     new Npc(200, 200, 50, 50, "Stefan", StefanImage),
     new Npc(300, 300, 50, 50, "Anton", AntonImage),
     new Npc(400, 400, 50, 50, "Gustav", GustavImage),
     new Npc(500, 500, 50, 50, "Sten", StenImage)
+    */
 ]
